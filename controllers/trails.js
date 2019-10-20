270d58e970c4226005bb4ef1aad4f18ecc6448a5
Brandon205 <brandonblack02@gmail.com>
2019-10-19 17:46:13 -0700

    I guess a / messed it up, but trail/:id works now

--- controllers/trails.js
+++ controllers/trails.js
@@ -11,24 +11,12 @@ router.get('/', function(req, res) { // GET to /trails > show all of the users s
   });
 });
 
-router.get('/:id', function(req, res) { // GET to /trails:id > shows more details on a specific trail
-  console.log(req.params.id);
-  db.trail.findOne({
-    where: {
-      idnum: req.params.id
-    }
-  }).then(function(foundTrail) {
-    // res.json(foundTrail.idnum);
-    let idNum = parseInt(foundTrail.idnum);
-    console.log(`${hpUrl}/get-trails-by-id?ids=${idNum}&key=${process.env.HIKING_PROJECT_KEY}/`);
-    axios.get(`${hpUrl}/get-trails-by-id?ids=${idNum}&key=${process.env.HIKING_PROJECT_KEY}/`);
-    // axios.get('https://www.hikingproject.com/data/get-trails-by-id?ids=7001822&key=200620206-94b8bca7fc597b4ea5b5fc0a1adc0de2');
-  }).then(function(trail) {
-    console.log(`LLLLLLLLLLLLLL ${trail}`);
-    res.json(trail);
-    // res.render('pages/showTrail', { trail: trail });
+router.get('/:id', function(req, res) {
+  axios.get(`${hpUrl}/get-trails-by-id?ids=${Number(req.params.id)}&key=${process.env.HIKING_PROJECT_KEY}`)
+  .then(function(trail) {
+    res.json(trail.data);
   })
-  .catch(err => console.log(`IIIIIIIIIIIIIIIII ${err}`));
+  .catch(err => console.log(err));
 });
 
 router.post('/', function(req, res) { // POST to /trails > add a new trail to the users saved list (Form on trails.ejs)
