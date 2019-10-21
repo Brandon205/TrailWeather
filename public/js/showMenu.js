54f8e9f99bdf39749ac836d96dbce5ad8e8e3a39
Brandon205 <brandonblack02@gmail.com>
2019-10-21 09:17:22 -0700

    Attempting skycons again

--- public/js/showMenu.js
+++
@@ -1,9 +0,0 @@
-function showMenu() {
-  let links = document.getElementById('links');
-  console.log(links);
-  if (links.style.display === 'block') {
-  links.style.display = 'none';
-} else {
-  links.style.display = 'block';
-}
-}
